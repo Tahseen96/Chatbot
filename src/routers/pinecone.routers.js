9e64fb8c8024ed1a829c986c03f240a19019f468
Tahseen96 <tahseenfarooq96@gmail.com>
2023-07-18 23:12:46 +0500

    chatbot

--- src/routers/pinecone.routers.js
+++ src/routers/pinecone.routers.js
@@ -1,6 +1,72 @@
-const express = require('express')
-const asyncHandler = require('express-async-handler');
-const router = new express.Router()
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const router = new express.Router();
+const {HuggingFaceInferenceEmbeddings} = require("langchain/embeddings/hf")
+const extract = require("pdf-text-extract");
+const { RecursiveCharacterTextSplitter } = require("langchain/text_splitter");
+const { PineconeClient } = require("@pinecone-database/pinecone");
+const { HuggingFaceInference } = require("langchain/llms/hf");
 
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 5000,
+  chunkOverlap: 100,
+});
 
-module.exports = router
+const hfEmbeddings = new HuggingFaceInferenceEmbeddings({
+  apiKey : "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"
+})
+
+let index = undefined;
+const pinecone = new PineconeClient();
+
+(async () => {
+  await pinecone.init({
+    environment: "us-west1-gcp-free",
+    apiKey: "50501fb1-cdea-456b-9df3-f9be007a0847",
+  });
+  index = pinecone.Index("index-pdf");
+})();
+
+const convertTextInChunks = async (raw_text) => {
+  const output = await splitter.createDocuments([raw_text]);
+  return output;
+};
+
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { caseId } = req.body;
+    extract("./4.pdf", { splitPages: false }, async (err, pages) => {
+      let pdfRawText = "";
+      let allPdfVectors = [];
+      if (err) {
+        res.send("Check the pdf file.");
+        return;
+      }
+      pages.forEach((page) => {
+        pdfRawText += page + "\n";
+      });
+      const rawTextInChunksInDocuments = await convertTextInChunks(pdfRawText);
+      for (let counter = 0; counter < rawTextInChunksInDocuments.length; counter++) {
+        
+        let documentEmbedding = await hfEmbeddings.embedQuery(rawTextInChunksInDocuments[counter].pageContent)
+        
+        allPdfVectors.push({
+          id: `vector-${counter}`,
+          values: documentEmbedding,
+          metadata: {
+            text: rawTextInChunksInDocuments[counter].pageContent,
+          },
+        });
+      }
+      let upsertRequest = {
+        vectors: allPdfVectors,
+        namespace:  "pdf4",
+      };
+      const upsertResponse = await index.upsert({ upsertRequest });
+      res.send(`Pinecone updated ${upsertResponse}`);
+    });
+  })
+);
+
+module.exports = router;
--- src/routers/query.routers.js
+++ src/routers/query.routers.js
@@ -1,11 +1,70 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 const router = new express.Router();
+const { PineconeClient } = require("@pinecone-database/pinecone");
+const { HuggingFaceInference } = require("langchain/llms/hf");
+const { PineconeStore } = require("langchain/vectorstores/pinecone");
+const { HuggingFaceInferenceEmbeddings } = require("langchain/embeddings/hf");
+const {VectorDBQAChain, RetrievalQAChain } = require("langchain/chains");
+const {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  PromptTemplate,
+  SystemMessagePromptTemplate,
+} = require("langchain/prompts")
 
-router.get(
+
+const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+  SystemMessagePromptTemplate.fromTemplate(
+    "You are AskMary and your task is to answer the question from the given data."
+  ),
+  HumanMessagePromptTemplate.fromTemplate("{text}"),
+]);
+
+const hfEmbeddings = new HuggingFaceInferenceEmbeddings({
+  apiKey: "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",
+});
+
+const pinecone = new PineconeClient();
+let index;
+let vectorStore;
+let chain;
+
+const model = new HuggingFaceInference({
+  model: "tiiuae/falcon-7b-instruct",
+  apiKey: "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", // In Node.js defaults to process.env.HUGGINGFACEHUB_API_KEY
+  maxTokens: 1000,
+  temperature: 0.1,
+  topK: 10
+});
+
+(async () => {
+  await pinecone.init({
+    environment: "us-west1-gcp-free",
+    apiKey: "50501fb1-cdea-456b-9df3-f9be007a0847",
+  });
+  index = pinecone.Index("index-pdf");
+
+  vectorStore = await PineconeStore.fromExistingIndex(hfEmbeddings, {
+    pineconeIndex: index,
+  });
+  
+  chain = VectorDBQAChain.fromLLM(model, vectorStore, {
+    k: 1,
+    returnSourceDocuments: true,
+  });
+})();
+
+router.post(
   "/",
   asyncHandler(async (req, res) => {
-    res.send("This is query router")
+    const { question } = req.body;
+
+    const chatbotResponse = await chain.call({
+      query: question,
+    });
+
+    res.send(chatbotResponse);
   })
 );
 
